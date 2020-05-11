5f8c1c787cc9e2a551917660f8b9f986a2c972a5
Sirb Sebi <sebisarb@yahoo.com>
2020-05-11 11:26:53 +0300

    Added Contact, Services pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { NewsComponent } from './news/news.component';
+import { PortofolioComponent } from './p/portofolio.component';
+import { ServicesComponent } from './services/services.component';
 
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  // { path: 'About', component: AboutComponent },
+  { path: 'Contact', component: ContactComponent },
+  { path: '', component: HomeComponent },
+  // { path: 'News', component: NewsComponent },
+  // { path: 'Portofolio', component: PortofolioComponent },
+  { path: 'Services', component: ServicesComponent },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+import { PortofolioComponent } from './p/portofolio.component';
+import { AboutComponent } from './about/about.component';
+import { NewsComponent } from './news/news.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ServicesComponent,
+    PortofolioComponent,
+    AboutComponent,
+    NewsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
